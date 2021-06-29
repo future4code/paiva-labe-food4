50a98f66bc2748e2e7301f54da5ae5944c54fdf5
Drilias <drica.e.f@hotmail.com>
2021-06-29 08:45:25 -0300

    adicionado Global Styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import theme from './constants/theme';
 import Router from './Routes/Router';
+import GlobalStyle from './globalStyles/GlobalStyles';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Router />
     </ThemeProvider>
   );
