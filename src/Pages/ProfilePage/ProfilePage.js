05c722801008f016fbfed88c137391f7b21e13a5
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-07-02 11:53:22 -0300

    Página Profile e Error

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import useProtectedPage from '../../hooks/useProtectedPage'
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import { setProfile} from "../../services/User"
+import useProtectedPage from "../../hooks/useProtectedPage";
 
-const ProfilePage = () => {
-    useProtectedPage()
+import {
+  ContainerProfilePage,
+  ContainerAddress,
+  ContainerUser,
+  ContainerOrderHistoric,
+} from "./styled";
 
-    return (
-        <div>ProfilePage</div>
-    )
-}
+export const ProfilePage = () => {
+  useProtectedPage();
+  const [profile, setProfile] = useState();
+  const history = useHistory();
 
-export default ProfilePage
+  return (
+    
+      <ContainerProfilePage>
+        <ContainerUser>
+          <p>Nome: {profile.name}</p>
+          <p>E-mail: {profile.email}</p>
+          <p>CPF: {profile.cpf}</p>
+        </ContainerUser>
+
+        <ContainerAddress>
+          <p>Endereço cadastrado:</p>
+          <p>{profile.address}</p>
+        </ContainerAddress>
+
+        <ContainerOrderHistoric>
+          <p>Histórico de pedidos</p>
+        </ContainerOrderHistoric>
+      </ContainerProfilePage>
+    
+  );
+};
---
+++ src/Pages/ProfilePage/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+export const ContainerProfilePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 70px;
+  font-size: 17px;
+`;
+
+export const ContainerUser = styled.div`
+
+`;
+
+
+export const ContainerAddress = styled.div`
+  width: 100vw;
+  height: 13vh;
+  margin-top: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const ContainerOrderHistoric = styled.div`
+
+`;
